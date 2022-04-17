f166efc5ce754cf75ebc6c7c48d0e11cb98c406a
triangle <849965307@qq.com>
2022-04-17 22:12:52 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,51 @@
+# docsify-codeLineNum
+
+# 作用
+
+为`code`的代码显示添加行号，并高亮显示激活行。
+
+
+<p style="text-align:center;"><img src="./image/show.jpg" width="80%" align="middle" /></p>
+
+# 安装
+
+在`index.html`中的`<head>`标签中添加：
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/spite-triangle/docsify-codeLineNum@1.0.0/css/codeLineNum.css"></script>
+```
+
+在`index.html`中的`<body>`标签中添加：
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/spite-triangle/docsify-codeLineNum@1.0.0/script/codeLineNum.min.js"></script>
+```
+
+# 配置
+在`index.html1`中，在`window.$docsify`设置代码块的黑名单，在该黑名单中的代码块，不会显示行号。
+```javascript
+    window.$docsify = {
+      codeLineNum: {
+        blacklist: ['python','java']
+      }
+    }
+```
+
+
+# 使用
+
+在`markdown`中写入代码块
+
+```
+    ```cpp
+        #include <stdio.h>
+        #include <iostream>
+        using namespace std;
+        void main(int argc, char *argv[]){
+            cout << "hello world" << endl;
+        }
+    ```
+```
+
+
+
---
+++ script/codeLineNum.js
@@ -0,0 +1,106 @@
+! function () {
+    window.$docsify.plugins = [].concat(window.$docsify.plugins, function (hook, vm) {
+
+        // 通过属性值捕获元素
+        var getElementByAttr = function (tag, dataAttr) {
+            var aElements = document.getElementsByTagName(tag);
+            var aEle = [];
+            for (var i = 0; i < aElements.length; i++) {
+                var ele = aElements[i].getAttribute(dataAttr).toLowerCase();
+            // 配置检测
+                if(window.$docsify.codeLineNum === undefined || window.$docsify.codeLineNum.blacklist === undefined) {
+                    aEle.push(aElements[i]);
+                    continue;
+                }
+
+                if (window.$docsify.codeLineNum.blacklist.indexOf(ele) == -1) {
+                    aEle.push(aElements[i]);
+                }
+            }
+            return aEle;
+        };
+
+        // 通过标签名，获取子元素内的第一个元素
+        var getFirstSubelementByTag = function (parent, tagName) {
+            // 没有子元素
+            if (parent.childNodes == undefined) {
+                return null;
+            }
+            var element = null;
+            for (var i = 0; i < parent.childNodes.length; i++) {
+                if (parent.childNodes[i].tagName != undefined && parent.childNodes[i].tagName == tagName) {
+                    element = parent.childNodes[i];
+                    break;
+                }
+            }
+            return element;
+        };
+
+        // 创建一行
+        var createLine = function (index, content) {
+            var line = "<tr>";
+            line += '<td id="L' + index + '" class="td-code td-code-line-number" data-line-number="' + index + '">';
+            line += '<span class="code-line-number">' + index + '</span>'
+            line += '</td>';
+            line += '<td id="LC' + index + '" class="td-code td-code-line-content">' + content + '</td>';
+            line += "</tr>"
+            return line;
+        };
+
+        // 重新规范配置参数
+        hook.init(function () {
+
+            // 配置检测
+            if ( window.$docsify.codeLineNum === undefined || window.$docsify.codeLineNum.blacklist == undefined) {
+                return;
+            }
+            window.$docsify.codeLineNum.blacklist.forEach(function (item, index) {
+                window.$docsify.codeLineNum.blacklist[index] = item.trim().toLowerCase();
+            });
+        });
+
+        // 添加行号
+        hook.doneEach(function () {
+            // 清空选中
+            window.$docsify["lastSelectedLine"] = null;
+
+            // 查找所有的 code，黑名单除外
+            var pres = getElementByAttr('pre', 'data-lang');
+
+            // 遍历所有的 code
+            pres.forEach(function (pre, index) {
+                var html = '';
+
+                // 获取 code 标签
+                var code = getFirstSubelementByTag(pre, 'CODE');
+                // 代码拆分行
+                var lines = code.innerHTML.split('\n');
+                for (var i = 0; i < lines.length; i++) {
+                    html += createLine(i + 1, lines[i]);
+                }
+                code.innerHTML = '<table class="code-table"><tbody width=100%>' + html + '</tbody></table>';
+                code.classList.add("code-block")
+                // 高亮行
+                var codeLineNums = code.querySelectorAll('.td-code-line-content');
+                codeLineNums.forEach(function (line) {
+
+                    // 行高亮
+                    line.addEventListener('mouseup', function (e) {
+                        // 是否为左键
+                        if (e.button != 0) {
+                            return;
+                        }
+                        // 查看之前有没有被选中的
+                        if (window.$docsify.lastSelectedLine != null && this != window.$docsify.lastSelectedLine) {
+                            window.$docsify.lastSelectedLine.classList.remove("code-line-highlight");
+                        }
+                        // 当前行被选中
+                        this.classList.add("code-line-highlight");
+                        window.$docsify.lastSelectedLine = this;
+                    });
+
+                });
+            });
+        });
+    });
+}();
